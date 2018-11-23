a3e2dbd99f6214109b7014e8fd6e200ed46e71d0
johnrusselmoleragarcia <asdsad@gmail.com>
2018-11-23 15:58:55 +0800

    Modified chat for error packet

--- proto_chat/chat.py
+++ proto_chat/chat.py
@@ -53,8 +53,16 @@ player.name = input("Player name: ")
 def join_Lobby(lobby_id):
 	print("Lobby ID: {}".format(lobby_id))
 	connect_packet = Connect(player.name,player.id,lobby_id)
-	print(connect_packet)
+	# print("halu")
+	print(tcp)
 	# os.system("stty -echo")
+	
+	if connect_packet.type == tcp.ERR_LDNE:
+		error_packet = tcp.ErrLdnePacket()
+
+		print("Lobby does not exist.\n")
+		return
+
 	while True:
 		sockets = [sys.stdin,s]
 		read_sockets,write_sockets,error_sockets = select.select(sockets,[],[])
@@ -71,17 +79,17 @@ def join_Lobby(lobby_id):
 					print(chat_packet.player.name + ": " + chat_packet.message)
 				elif tcp.type == tcp.CONNECT:
 					connect_packet.ParseFromString(data)
-					print(connect_packet.player.name + " has joined the lobby")
+					print(player.name + " has joined the lobby")
 				elif tcp.type == tcp.DISCONNECT:
 					connect_packet.ParseFromString(data)
-					print(connect_packet.player.name + " has disconnected from lobby")
+					# print(player.name + " has disconnected from lobby")
 				elif tcp.type == tcp.PLAYER_LIST:
 					playerList_packet = tcp.PlayerListPacket()
 					playerList_packet.ParseFromString(data)
+					print("List of Players in the lobby")
 					for players in playerList_packet.player_list:
-						print("List of Players in the lobby")
-						print(playerList_packet.player.name)
-
+						print(players.name)
+				
 			else:
 				message = sys.stdin.readline().rstrip('\n')
 
@@ -89,7 +97,9 @@ def join_Lobby(lobby_id):
 					disconnect_packet = tcp.DisconnectPacket()
 					disconnect_packet.type = tcp.DISCONNECT
 					s.sendall(disconnect_packet.SerializeToString())
+					# socket.close()
 					return
+					# break
 				elif message == "player-list":
 					playerList_packet = tcp.PlayerListPacket()
 					playerList_packet.type = tcp.PLAYER_LIST
